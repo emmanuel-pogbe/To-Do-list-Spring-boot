a331ad2cf03e943720d39941d3a667cb2be4a3bb
Pogbe Emmanuel <mauyonpogbe@gmail.com>
2026-03-23 11:35:14 +0100

    Initial commit

---
+++ src/test/java/com/shopleft/todo/ToDoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopleft.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
